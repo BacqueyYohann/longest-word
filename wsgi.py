620f17c4d2f309b7c490c2d5897f371a30a6d1fd
yohann bacquey <yohann.bacquey@gmail.com>
2024-02-22 19:45:56 +0000

    high score basic implementation

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,14 @@
 # wsgi.py
 # pylint: disable=missing-docstring
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+from flask_session import Session
 from longest_word.game import Game
 
 app = Flask(__name__)
+SESSION_TYPE='filesystem'
+app.config.from_object(__name__)
+Session(app)
 
 @app.route('/')
 def home():
@@ -16,5 +20,17 @@ def check():
     game = Game()
     game.grid = list(request.form['grid'])
     word = request.form['word']
-    is_valid = game.is_valid(word)
-    return render_template('check.html', is_valid=is_valid, grid=game.grid, word=word)
+    if game.is_valid(word) :
+        is_valid = game.is_valid(word)
+        set(word)
+    #is_valid = game.is_valid(word)
+    return render_template('check.html', is_valid=is_valid, grid=game.grid, word=word, high_score = get())
+
+def set(word):
+    session['high_score'] = len(word)
+    return
+
+def get():
+    if not session.get('high_score') :
+        return 0
+    return session.get('high_score')
